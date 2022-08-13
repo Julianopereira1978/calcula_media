b41103c95859225db49394b041871d5a0642da09
Juliano Antonio de Abreu Pereira <jubamarvim@yahoo.com.br>
2022-08-13 19:37:36 -0300

    Add files via upload

---
+++ calcula_media/calcula_media.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 13 19:13:36 2022
+
+@author: Juliano
+"""
+import os
+
+nota1 = float(input('Informe a primeira nota: '))
+
+nota2 = float(input('Informe a segunda nota: '))
+
+nota3 = float(input('Informe a terceira nota: '))
+
+media = (nota1 + nota2 + nota3)/3
+
+if media >= 7.0:
+
+    print('Aprovado')
+    print('Boas férias')
+else:
+    print('Reprovado')
+    
+os.system('pause')
